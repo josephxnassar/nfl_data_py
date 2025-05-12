1c71888f835fbddf75b39aeedcc0aff84541c9e0
josephxnassar <ajn619@outlook.com>
2025-05-12 16:25:36 -0700

    Added type definitions

--- tests/regression_test.py
+++ tests/regression_test.py
@@ -12,7 +12,7 @@ def sample_data():
     y = pd.Series([10, 20, 30, 40, 50])
     return X, y
 
-def test_train_ridge_regression_returns_model_and_score(sample_data):
+def test_train_ridge_regression_returns_model_and_score(sample_data: tuple):
     X, y = sample_data
 
     reg = Regression(X, y)
@@ -21,7 +21,7 @@ def test_train_ridge_regression_returns_model_and_score(sample_data):
     assert hasattr(model, 'coef_')
     assert isinstance(score, float)
 
-def test_weights_are_computed_correctly(sample_data):
+def test_weights_are_computed_correctly(sample_data: tuple):
     X, y = sample_data
 
     reg = Regression(X, y)
@@ -30,7 +30,7 @@ def test_weights_are_computed_correctly(sample_data):
     assert isinstance(weights, dict)
     assert all(col in weights for col in X.columns)
 
-def test_calculate_rating_outputs_series(sample_data):
+def test_calculate_rating_outputs_series(sample_data: tuple):
     X, y = sample_data
 
     reg = Regression(X, y)
@@ -39,11 +39,11 @@ def test_calculate_rating_outputs_series(sample_data):
     assert isinstance(rating_series, pd.Series)
     assert len(rating_series) == len(X)
 
-def test_get_ratings_adds_rating_column(sample_data):
+def test_get_ratings_adds_rating_column(sample_data: tuple):
     X, y = sample_data
 
     reg = Regression(X.copy(), y)
     rated_df = reg.get_ratings()
-    
+
     assert 'rating' in rated_df.columns
     assert rated_df['rating'].is_monotonic_decreasing or rated_df['rating'].is_monotonic_increasing
